f479aa3b8405f9b287d40a8d2bffe7b92652497e
Nouu Choi <nouu30133@naver.com>
2025-04-13 19:08:00 +0900

    Day 2: k8s 컨트롤 플레인 노드 vagrant 구성 완료

---
+++ .vagrant/bundler/global.sol
@@ -0,0 +1,1 @@
+{"dependencies":[["vagrant-disksize",["= 0.1.3"]],["net-ssh",[">= 2.6.5","< 8.0.0"]],["net-scp",[">= 1.1"]],["log4r",["~> 1.1"]],["vagrant-scp",["= 0.5.9"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.32.0"]]],"checksum":"58942e21b47535967c75ba5133417512f48da99feef1ba35885a0eebba393947","vagrant_version":"2.4.3"}
